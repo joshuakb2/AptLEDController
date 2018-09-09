36d28dea5d256b877a17a94ce47750e9c2271201
Joshua Baker <joshuakb2@gmail.com>
2018-09-09 17:32:40 -0500

    Added support for client socket connections.
    Messages that are received along with a valid token will be forwarded
    to every connected client.
    
    Also fixed the makefile and improved the directory structure.

--- server/generateToken.sh
+++ server/generateToken.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo 'Generating a random hexadecimal string token. This will take a while....'
+echo 'Generating a random hexadecimal string token. This may take a while....'
 echo
 
 exec hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random
---
+++ server/ts/DataForwarder.ts
@@ -0,0 +1,71 @@
+import * as net from 'net';
+
+export class DataForwarder {
+    private sockets: Set<net.Socket>;
+    private disposing: boolean;
+
+    constructor() {
+        this.sockets = new Set<net.Socket>();
+        this.disposing = false;
+    }
+
+    registerSocket(s: net.Socket) {
+        if (!this.disposing)
+            this.sockets.add(s);
+    }
+
+    unregisterSocket(s: net.Socket) {
+        this.sockets.delete(s);
+    }
+
+    forward(message: string, cb?: Function) {
+        var leftToSend: number = this.sockets.size;
+
+        var sent = (): void => {
+            leftToSend--;
+
+            if (leftToSend === 0) {
+                if (cb)
+                    cb();
+            }
+        };
+        
+        if (!this.disposing) {
+            console.log('Forwarding "' + message + '" to ' + this.sockets.size + ' client' + (this.sockets.size === 1 ? '' : 's') + '.');
+
+            this.sockets.forEach(function(s: net.Socket) {
+                s.write(message, sent);
+            });
+        }
+        else
+            throw new Error('Cannot send a message after DataForwarder.dispose() has been called.');
+    }
+
+    dispose(cb?: Function) {
+        var leftToClose: number = this.sockets.size;
+
+        var closed = (): void => {
+            leftToClose--;
+
+            if (leftToClose === 0) {
+                this.sockets.clear();
+                this.disposing = false;
+
+                if (cb)
+                    cb();
+            }
+        };
+
+        if (!this.disposing) {
+            this.disposing = true;
+
+            if (this.sockets.size === 0)
+                closed();
+
+            this.sockets.forEach(function(s: net.Socket) {
+                s.on('close', closed);
+                s.end();
+            });
+        }
+    }
+}
